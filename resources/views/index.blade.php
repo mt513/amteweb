4bc09561897d80949376423dbea6033a0e7c7135
mt513 <tanasem90@gmail.com>
2020-06-10 04:36:40 +0300

    mobile

---
+++ resources/views/index.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<head>
+    <title>AMTE WEB</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type= "text/css" href="{{URL::asset('css/sk.css')}}">
+</head>
+
+<body>
+    <div class="wrapper">
+        <header class="header p-1 flex-row jusitfy-content-space-between">
+            <div class="flex row">
+            <div class="logo">
+                <div class="sq1">
+                </div>
+                <div class="sq2">
+                </div>
+                <div class="sq3">
+                </div>
+                <div class="sq4">
+                </div>
+            </div>
+            <h4 class="p-l-1 align-self-center scolor m-0 fw100">AMTE WEB</h4>
+            </div>
+            <div class="menumob">
+                <div class="menuline">
+                </div>
+                <div class="menuline">
+                </div>
+                <div class="menuline">
+                </div>
+            </div>
+        </header>
+        
+        <h1 class="m-1">Content Here</h1>
+        <h2>Content Here</h2>
+        <h3>Content Here</h3>
+        <h4>Content Here</h4>
+    </div>
+</body>
+</html>
