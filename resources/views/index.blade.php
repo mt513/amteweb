dd620c49d4e90470a4f3c61603e18b3e3216c1ef
mt513 <tanasem90@gmail.com>
2020-06-12 14:00:58 +0300

    header, main

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,25 +1,26 @@
 <!DOCTYPE html>
+
 <head>
     <title>AMTE WEB</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type= "text/css" href="{{URL::asset('css/sk.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/sk.css') }}">
 </head>
 
 <body>
     <div class="wrapper">
-        <header class="header p-1 flex-row jusitfy-content-space-between">
-            <div class="flex row">
-            <div class="logo">
-                <div class="sq1">
+        <header class="header flex align-items-center p-1 flex-row justify-content-space-between">
+            <div class="flex row align-items-center">
+                <div class="logo">
+                    <div class="sq1">
+                    </div>
+                    <div class="sq2">
+                    </div>
+                    <div class="sq3">
+                    </div>
+                    <div class="sq4">
+                    </div>
                 </div>
-                <div class="sq2">
-                </div>
-                <div class="sq3">
-                </div>
-                <div class="sq4">
-                </div>
-            </div>
-            <h4 class="p-l-1 align-self-center scolor m-0 fw100">AMTE WEB</h4>
+                <h4 class="p-l-1 align-self-center scolor m-0 fw400">AMTE WEB</h4>
             </div>
             <div class="menumob">
                 <div class="menuline">
@@ -30,11 +31,18 @@
                 </div>
             </div>
         </header>
-        
-        <h1 class="m-1">Content Here</h1>
-        <h2>Content Here</h2>
-        <h3>Content Here</h3>
-        <h4>Content Here</h4>
+        <main class="p-0 m-0">
+            <div class="fpage flex flex-column align-items-center">
+                <h1 class="m-0 p-t-2">Creative Solutions</h1>
+                <h4 class="m-0 p-t-2 fw300 tac">A wide spectrum of solutions<br>for your tasks and ideas.</h2>
+                <img class="m-t-2 p-b-2" src="{{URL::asset('img/homecentercolored.png')}}">
+              
+            </div>
+ <p class="p-t-1 tac m-0" style="font-size:1rem;"> <q><i>Our success is measured by our achievments</i></q></p>
+
+        </main>
+
     </div>
 </body>
+
 </html>
