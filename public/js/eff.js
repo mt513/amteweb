25e3e5b068d74921e4a839012cd348dc97785bec
mt513 <tanasem90@gmail.com>
2020-08-03 08:13:12 +0300

    Corrected js file fod calcuating the vh

--- public/js/eff.js
+++ public/js/eff.js
@@ -65,15 +65,19 @@ function smoothScroll(eID){
 
 window.addEventListener("resize",setsbitems);
 
-let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 
 var restimer;
 var sbitems = document.querySelectorAll('.sbitem');
 function setsbitems(){
 
+  
+
+
     clearTimeout(restimer);
     restimer=setTimeout(function(){
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
         var max=0; 
         for (let i=0; i<sbitems.length; i++){
             sbitems[i].removeAttribute("style");
